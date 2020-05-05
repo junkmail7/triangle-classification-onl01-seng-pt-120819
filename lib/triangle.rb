52844098fa475d536964e540709cf434979f142d
William Pannell <lpann5@gmail.com>
2020-05-05 17:56:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(s1, s2, s3)
+    @side1 = s1
+    @side2 = s2
+    @side3 = s3
+  end
+
+  def kind
+    if @side1==@side2&&@side1==@side3&&@side2==@side3
+        return :equilateral
+      elsif @side1==@side2&&@side1==@side3&&@side2==@side3
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
